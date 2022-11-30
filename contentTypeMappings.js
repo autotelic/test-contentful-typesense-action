19fec971df7076879bdd129600602b6e53a5faf2
Edward Stone <ed@autotelic.com>
2022-11-29 21:55:45 -0800

    property locations from buildings

--- contentTypeMappings.js
+++ contentTypeMappings.js
@@ -1,5 +1,29 @@
 const contentTypeMappings = {
-  property: {}
+  building: {},
+  property: {
+    extraFields: [
+      { name: 'locations', type: 'geopoint[]' }
+    ],
+    fieldMappings: {
+      locations: (document, allEntities, locale) => {
+        const { fields } = document
+        const { propertyBuilding: buildings } = fields
+        const buildingEntities = allEntities['building']
+        if (buildings === undefined) return []
+        return buildings[locale].filter(ref => {
+          const building = buildingEntities[ref.sys.id]
+          if (building === undefined) return false
+          const { fields: { buildingLocation } } = building
+          if (buildingLocation === undefined) return false
+          return true
+        }).map(ref => {
+          const building = buildingEntities[ref.sys.id]
+          const { fields: { buildingLocation } } = building
+          return Object.values(buildingLocation[locale]).reverse()
+        })
+      }
+    }
+  }
 }
 
 module.exports = contentTypeMappings
